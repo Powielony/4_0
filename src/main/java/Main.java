fe4a1fed6962453a918be8d1a3e6d1a7dbef3e60
Krzysztof Lech <puciekmaster@yahoo.com>
2025-04-16 12:09:37 +0000

    Commit 4_2: zadanie 4_2 (przed 4_1)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,18 +8,45 @@ Kod bazowy programu Commit4_0:
 */
 
 import java.io.IOException;
+import java.util.Scanner;
 
 class Main {
   public static void main(String[] args) {
     try {
       Service s = new Service();
-      s.addStudent(new Student("Krzysztof", 20));
-      s.addStudent(new Student("Janusz", 40));
+      Scanner in = new Scanner(System.in);
 
-      var students = s.getStudents();
-      for(Student current : students) {
-        System.out.println(current.ToString());
+      int wybor;
+
+      while (true) {
+        System.out.println("Proszę wybrać opcję");
+        System.out.println("1. Dodaj studenta");
+        System.out.println("2. Wypisz zapisanych studentów");
+        System.out.println("3. Wyszukaj studenta po imieniu");
+        System.out.println("0. Zamknij program");
+        System.out.print("Wybór: ");
+        wybor = in.nextInt();
+        System.out.println();
+
+        switch (wybor) {
+          case 1:
+            s.addStudent(new Student(name, age));
+            break;
+          case 2:
+            var students = s.getStudents();
+            for (Student current : students) {
+              System.out.println(current.ToString());
+            }
+            break;
+          case 3:
+            System.out.println("Not implemented yet!");
+            break;
+          case 0:
+            System.exit(0);
+        }
+
       }
+
     } catch (IOException e) {
 
     }
